2575a77bf30fd2d02726eea5e78995d31afa120e
Jan Sowa <jan.piotr.sowa@gmail.com>
2016-09-04 16:03:07 +0200

    Zadanie 4 z rozdzialu 2

---
+++ rozdzial2/zadanie4.ml
@@ -0,0 +1,13 @@
+(*
+*Napisz procedurę, która sprawdza, czy dana liczba jest pierwsza.
+*)
+
+let rec isPrime1pom a n =
+  if ((n<4) || (a=n)) then true
+  else
+   (if ((n mod a)=0) then false
+   else isPrime1pom (a+1) n);;
+
+let isPrime1 n = isPrime1pom 2 n;;
+
+exit 0;;
