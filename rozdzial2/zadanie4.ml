923ce58d90c9df339d186578a14e765ae1fb98d3
Jan Sowa <jan.piotr.sowa@gmail.com>
2016-09-19 09:04:40 +0200

    Poprawka zadania 4 z rozdzialu 2

--- rozdzial2/zadanie4.ml
+++ rozdzial2/zadanie4.ml
@@ -3,11 +3,12 @@
 *)
 
 let rec isPrime1pom a n =
-  if ((n<4) || (a=n)) then true
-  else
-   (if ((n mod a)=0) then false
-   else isPrime1pom (a+1) n);;
+(*    if ((n<4) || (a=n)) then true
+	else if (n==1) then false*)
+    if n=1 then false
+    else if (n<4) || (a=n) then true
+    else
+    (if ((n mod a)=0) then false
+    else isPrime1pom (a+1) n);;
 
 let isPrime1 n = isPrime1pom 2 n;;
-
-exit 0;;
